9da847c531364f35d3f0d25c6b1f33d8dd75cc64
Siddheshk15 <you@example.com>
2014-12-29 11:16:53 +0530

    initial commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 SMP PREEMPT Sat Jun 7 14:29:19 CST 2014"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "HT010_UBUNTU_64"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
